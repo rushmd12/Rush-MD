92dfaca1fc8daf14537a62c14c5def5d674996e8
rushmd12 <dilshanrashmika141rs12@gmail.com>
2024-07-04 19:44:38 +0530

    Update call-handler.js

--- src/event/call-handler.js
+++ src/event/call-handler.js
@@ -4,7 +4,7 @@ const Callupdate = async (json, sock) => {
    for (const id of json) {
       if (id.status === 'offer' && config.REJECT_CALL ) {
          let msg = await sock.sendMessage(id.from, {
-            text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
+            text: `*_⚙️ Auto Reject Activated MODE කර ඇත_* \n*_🔍ලබා ගත නොහැක📵_*`,
             mentions: [id.from],
          });
          await sock.rejectCall(id.id, id.from);
